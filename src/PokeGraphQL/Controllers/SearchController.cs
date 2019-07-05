f9c37481a65ef5e83e5e58f43362f4e1a76b8a0f
Jose Toledo <jotatoledon@gmail.com>
2019-07-05 18:57:53 +0200

    Add project files.

---
+++ src/PokeGraphQL/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿namespace PokeGraphQL.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Nest;
+
+    [Route("[controller]")]
+    public class SearchController : Controller
+    {
+        private readonly IElasticClient _elasticClient;
+
+        public SearchController(IElasticClient elasticClient)
+        {
+            this._elasticClient = elasticClient;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Search(string query, int page = 1, int pageSize = 5)
+        {
+            var response = await this._elasticClient.SearchAsync<dynamic>(s => s.AllIndices().Query(q => q.QueryString(qs => qs.Query(query)))
+                        .From((page - 1) * pageSize)
+                        .Size(pageSize));
+
+            if (!response.IsValid)
+            {
+                // We could handle errors here by checking response.OriginalException or response.ServerError properties
+                return this.NoContent();
+            }
+
+            //if (page > 1)
+            //    ViewData["prev"] = GetSearchUrl(query, page - 1, pageSize);
+            //if (response.IsValid && response.Total > page * pageSize)
+            //    ViewData["next"] = GetSearchUrl(query, page + 1, pageSize);
+
+            return this.Ok(response.Documents);
+        }
+    }
+}
---
+++ src/PokeGraphQL/ElasticsearchExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace PokeGraphQL
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Nest;
+
+    internal static class ElasticsearchExtensions
+    {
+        internal static void AddElasticsearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration["elasticsearch:url"];
+            var defaultIndex = configuration["elasticsearch:index"];
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .DefaultIndex(defaultIndex);
+                //.DefaultMappingFor<Post>(m => m
+                //    .Ignore(p => p.IsPublished)
+                //    .PropertyName(p => p.ID, "id")
+                //)
+                //.DefaultMappingFor<Comment>(m => m
+                //    .Ignore(c => c.Email)
+                //    .Ignore(c => c.IsAdmin)
+                //    .PropertyName(c => c.ID, "id")
+                //);
+
+            services.AddSingleton<IElasticClient>(s => new ElasticClient(settings));
+        }
+    }
+}
