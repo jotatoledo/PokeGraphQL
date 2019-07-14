4d59d13dab527506f2f3e734f807efbef072c763
Jose Toledo <jotatoledon@gmail.com>
2019-07-14 14:25:08 +0200

    feat: complete contest resource types

--- src/PokeGraphQL/GraphQL/Resources/Contests/ContestEffectType.cs
+++ src/PokeGraphQL/GraphQL/Resources/Contests/ContestEffectType.cs
@@ -15,14 +15,17 @@ namespace PokeGraphQL.GraphQL.Resources.Contests
         /// <inheritdoc/>
         protected override void ConcreteConfigure(IObjectTypeDescriptor<ContestEffect> descriptor)
         {
-            // TODO add missing props: effect_entries, flavor_text_entries
-            // TODO remove next
-            descriptor.BindFields(BindingBehavior.Explicit);
             descriptor.Description("Contest effects refer to the effects of moves when used in contests.");
             descriptor.Field(x => x.Appeal)
                 .Description("The base number of hearts the user of this move gets.");
             descriptor.Field(x => x.Jam)
                 .Description("The base number of hearts the user's opponent loses.");
+            descriptor.Field(x => x.Effects)
+                .Description("The result of this contest effect listed in different languages.")
+                .Type<ListType<EffectType>>();
+            descriptor.Field(x => x.FlavorTexts)
+                .Description("The flavor text of this contest effect listed in different languages.")
+                .Type<ListType<FlavorTextType>>();
         }
     }
 }
--- src/PokeGraphQL/GraphQL/Resources/Contests/SuperContestEffectType.cs
+++ src/PokeGraphQL/GraphQL/Resources/Contests/SuperContestEffectType.cs
@@ -7,20 +7,34 @@
 
 namespace PokeGraphQL.GraphQL.Resources.Contests
 {
+    using System.Linq;
+    using System.Threading.Tasks;
     using HotChocolate.Types;
     using PokeAPI;
+    using PokeGraphQL.GraphQL.Resources.Moves;
 
     internal sealed class SuperContestEffectType : BaseApiObjectType<SuperContestEffect>
     {
         /// <inheritdoc/>
         protected override void ConcreteConfigure(IObjectTypeDescriptor<SuperContestEffect> descriptor)
         {
-            // TODO add missing props: flavor_text_entries, moves
-            // TODO remove next
-            descriptor.BindFields(BindingBehavior.Explicit);
             descriptor.Description("Super contest effects refer to the effects of moves when used in super contests.");
             descriptor.Field(x => x.Appeal)
                 .Description("The level of appeal this super contest effect has.");
+            descriptor.Field(x => x.Moves)
+                .Description("The result of this contest effect listed in different languages.")
+                .Type<ListType<MoveType>>()
+                .Resolver((ctx, token) =>
+                {
+                    var resolver = ctx.Service<MoveResolver>();
+                    var resourceTasks = ctx.Parent<SuperContestEffect>()
+                        .Moves
+                        .Select(move => resolver.GetMoveAsync(move.Name, token));
+                    return Task.WhenAll(resourceTasks);
+                });
+            descriptor.Field(x => x.FlavorTexts)
+                .Description("The flavor text of this contest effect listed in different languages.")
+                .Type<ListType<FlavorTextType>>();
         }
     }
 }
